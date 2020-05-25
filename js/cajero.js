34a8d265d6934f0da93b05f1329e05728024935f
ChristianHumpiri <christianquispe@upeu.edu.pe>
2020-05-25 12:11:58 -0500

    terminado

--- js/cajero.js
+++ js/cajero.js
@@ -15,11 +15,7 @@ function login(){
     if(entro==123){
        
         document.getElementById("p1").style.display="none";
-        
-        document.getElementById("p2").style.display="block";
-        
-
-        
+        document.getElementById("p2").style.display="block"; 
     }else{
         document.getElementById("mensaje").innerHTML="Contraseña incorrecta";
     }
@@ -27,17 +23,27 @@ function login(){
 }
 function retiro(){
     document.getElementById("p2").style.display="none";
-     document.getElementById("p3").style.display="block";
+    document.getElementById("p3").style.display="block";
      }
      
- function retiro2(){
+function retiro2(){
     document.getElementById("p3").style.display="none";
     document.getElementById("p4").style.display="block";
     }
-    function retiro3(){
-        document.getElementById("p4").style.display="none";
-        document.getElementById("p5").style.display="block";
+function retiro3(){
+    document.getElementById("p4").style.display="none";
+    document.getElementById("p5").style.display="block";
+    document.getElementById("num").style.display="none";
+    document.getElementById("num2").style.display="block";
         }
+function retiro6(){
+    document.getElementById("p6").style.display="none";
+    document.getElementById("p5").style.display="block";
+}
+function retiro5(){
+    document.getElementById("p6").style.display="none";
+    document.getElementById("p7").style.display="block";
+}
 document.getElementById("t1").addEventListener("click",t1);
 function t1(){ 
     let result=document.getElementById("t1").innerHTML;
@@ -101,15 +107,104 @@ function t9(){
     var value=nume.value;
     nume.value=value + result;
  }
- document.getElementById("t0").addEventListener("click",t5);
+ document.getElementById("t0").addEventListener("click",t0);
 function t0(){ 
     let result=document.getElementById("t0").innerHTML;
     var nume=document.getElementById("per");
     var value=nume.value;
     nume.value=value + result;
  }
- 
-       
+ document.getElementById("s1").addEventListener("click",s1);
+function s1(){ 
+    let result=document.getElementById("s1").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s2").addEventListener("click",s2);
+function s2(){ 
+    let result=document.getElementById("s2").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s3").addEventListener("click",s3);
+function s3(){ 
+    let result=document.getElementById("s3").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s4").addEventListener("click",s4);
+function s4(){ 
+    let result=document.getElementById("s4").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s5").addEventListener("click",s5);
+function s5(){ 
+    let result=document.getElementById("s5").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s6").addEventListener("click",s6);
+function s6(){ 
+    let result=document.getElementById("s6").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s7").addEventListener("click",s7);
+function s7(){ 
+    let result=document.getElementById("s7").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s8").addEventListener("click",s8);
+function s8(){ 
+    let result=document.getElementById("s8").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s9").addEventListener("click",s9);
+function s9(){ 
+    let result=document.getElementById("s9").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ document.getElementById("s0").addEventListener("click",s0);
+function s0(){ 
+    let result=document.getElementById("s0").innerHTML;
+    var nume=document.getElementById("total");
+    var value=nume.value;
+    nume.value=value + result;
+ }
+ function dinero() {
+    var montoactual;
+    var retiro=document.getElementById("total").value,montoactual = 5000;
+
+    if (retiro=="") {
+        document.getElementById("error").innerHTML="Ingresar Monto";
+    } else {
+        if (retiro%10==0) {
+            if (montoactual>retiro) {
+                document.getElementById("total").value=""
+                document.getElementById("p5").style.display="none";
+                document.getElementById("p6").style.display="block";
+            } else {
+                document.getElementById("error").innerHTML="Sin saldo";
+            }
+        } else {
+            document.getElementById("error").innerHTML="Solo billetes";
+        }
+    }
+}
+
 
 
  
